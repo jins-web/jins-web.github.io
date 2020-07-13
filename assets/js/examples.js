a79f60e458beb3ad93f8d1935364a790d53b170b
jin4333 <vhfhflsksk@gmail.com>
2020-07-13 10:56:29 +0900

    수정
    수정

--- assets/js/examples.js
+++ assets/js/examples.js
@@ -1,15 +1,31 @@
-$(document).ready(function(){
-    $( window ).scroll( function() {
-        if ( $( this ).scrollTop() > 300 ) {
-            $( '.top' ).fadeIn();
-            $('.header-wrap').addClass('nav-back')
-        } else {
-            $( '.top' ).fadeOut();
-            $('.header-wrap').removeClass('nav-back')
-        }
-    });
-    $( '.top' ).click( function() {
-        $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
-        return false;
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".top").fadeIn();
+      $(".header-wrap").addClass("nav-back");
+    } else {
+      $(".top").fadeOut();
+      $(".header-wrap").removeClass("nav-back");
+    }
+  });
+  $(".top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 400);
+    return false;
+  });
+  /* map height 조절 */
+  var id;
+  $(window)
+    .resize(function () {
+      clearTimeout(id);
+      id = setTimeout(doneResizing, 50);
+    })
+    .resize();
+
+  function doneResizing() {
+    $height = $(".map-size").height();
+    $width = $(".map-size").width();
+    $newHeight = Math.floor($height - 385);
+
+    $(".map-size").height($newHeight);
+  }
 });
